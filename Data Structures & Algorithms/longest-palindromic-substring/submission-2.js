a3d77e7b4ddbaa2143723abd206b9b6c97bcd977
longlostt <116051207+longlostt@users.noreply.github.com>
2026-03-31 01:12:32 -0400

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let maxPal = "";
+        for(let i = 0; i < s.length; i++){
+            isPalindrome(i,i);
+            isPalindrome(i,i+1);
+        }
+
+        function isPalindrome(l,r){
+            let curPal = l === r ? s[l] : "";
+            while(l >= 0 && r < s.length && s[l] === s[r]){
+                if(l === r) curPal = s[l];
+                else curPal = s[l] + curPal + s[r];
+                l--;
+                r++;
+            }
+            if(curPal.length > maxPal.length){
+                maxPal = curPal;
+            }
+        }
+        return maxPal;
+    }
+}
