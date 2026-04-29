b449107eaf57b0b65ac921b7059585e25cdd34df
longlostt <116051207+longlostt@users.noreply.github.com>
2026-04-29 11:44:25 -0400

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::unordered_map<int, int> freq;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            freq[nums[i]]++;
+        }
+
+        for(const auto& [key, value] : freq){
+            if(value > std::floor(nums.size()/2)) return key;
+        }
+    }
+};
