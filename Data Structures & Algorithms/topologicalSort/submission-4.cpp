eaebd8d66a9aa159fc962b8c94d4124f4dcdb567
longlostt <116051207+longlostt@users.noreply.github.com>
2026-05-07 01:22:05 -0400

    Add: topologicalSort - submission-4

---
+++ Data Structures & Algorithms/topologicalSort/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> topologicalSort(int n, vector<vector<int>>& edges) {
+        std::unordered_map<int, int> indegree;
+        std::unordered_map<int, std::vector<int>> adj;
+
+        for(vector<int> pairs: edges){
+            adj[pairs[0]].push_back(pairs[1]);
+            indegree[pairs[0]];
+            indegree[pairs[1]]++;
+        }
+
+        std::queue<int> q;
+        std::vector<int> res;
+
+        for(int i = 0; i < n; ++i){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        
+        while(!q.empty()){
+            int top = q.front(); q.pop();
+            res.push_back(top);
+            for(int v : adj[top]){
+                if(--indegree[v] == 0) q.push(v);
+            }
+
+        }
+        return res.size() == n ? res : std::vector<int>{};
+    }
+};
