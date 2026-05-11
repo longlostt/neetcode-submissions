8a083d93dd5f0b3cf38b148b8a726e87896e3a37
longlostt <116051207+longlostt@users.noreply.github.com>
2026-05-11 18:25:28 -0400

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   private:
+    vector<int> res;
+
+   public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        dfs(root);
+        return this->res;
+    }
+    void dfs(TreeNode* root) {
+        if (root == nullptr) return;
+        dfs(root->left);
+        this->res.push_back(root->val);
+        dfs(root->right);
+    }
+};
