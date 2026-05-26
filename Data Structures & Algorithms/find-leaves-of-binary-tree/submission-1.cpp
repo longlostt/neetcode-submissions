ce2079ef3bb9c6c0ec5439232731b44ef5f69a78
longlostt <116051207+longlostt@users.noreply.github.com>
2026-05-26 06:39:54 -0400

    Add: find-leaves-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/find-leaves-of-binary-tree/submission-1.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    int dfs(TreeNode* root){
+        if (!root) return -1;
+        int idx = max(dfs(root->left), dfs(root->right))+1;
+        idx == res.size() ? res.push_back({root->val}) : res[idx].push_back(root->val);
+        return idx;
+    }
+
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
