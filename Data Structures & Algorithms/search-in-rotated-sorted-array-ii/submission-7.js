ac5cfd12e98247115d663f4e1886adf4fc68f4b1
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-13 21:53:52 -0400

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let l = 0
+        let r = nums.length - 1;
+        while(l < r){
+            let mid = Math.floor(l + (r-l) / 2);
+            if(nums[mid] == target) return true;
+            if(nums[l] < nums[mid]){
+                if(target >= nums[l] && target <= nums[mid]) r = mid-1;
+                else l = mid+1;
+            } else if (nums[l] > nums[mid]) {
+                if(target <= nums[r] && target >= nums[mid]) l = mid+1;
+                else r = mid-1;
+            } else {
+                l++;
+            }
+        }
+        return nums[l] == target;
+    }
+}
