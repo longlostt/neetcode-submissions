effe032ed78eaf83532f27e6e228ce10fed16aa0
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-15 20:54:36 -0400

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        let visited = new Array(n).fill(false);
+        let adj = new Map();
+        let valid = true;
+
+        function dfs(val, prev){
+            visited[val] = true;
+            for(let newVal of adj.get(val)){
+                if(visited[newVal] == true){
+                    if(newVal != prev) valid = false;
+                } else {
+                    dfs(newVal, val);
+                }
+            }
+        }
+
+        for (let i = 0; i < n; i++) {
+            adj.set(i, []);
+        }
+        for (const [key, val] of edges) {
+            adj.get(key).push(val);
+            adj.get(val).push(key);
+        }
+        dfs(0, undefined);
+        
+        return valid == true && !visited.includes(false);
+    }
+}
