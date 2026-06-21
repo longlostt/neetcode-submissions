00945ff3be67bbb1844d5cf33c90fef6f1c73dd2
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-21 00:40:33 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        if (nums.length == 0) return [[]];
+        let res = [];
+        function build(perm, used) {
+            if (perm.length == nums.length) {
+                res.push([...perm]);
+                return;
+            }
+            for (let num of nums) {
+                if (!used.has(num)) {
+                    used.add(num);
+                    perm.push(num);
+                    build(perm, used);
+                    used.delete(num);
+                    perm.pop();
+                }
+            }
+        }
+        build([], new Set());
+        return res;
+    }
+}
