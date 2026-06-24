0b1d0e5bef36a9f35cc2d71b2b0f72c047fa510d
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-24 15:41:50 -0400

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-2

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    specialArray(nums) {
+        for(let i = 0; i <= Math.max(...nums); i++){
+            let x = i;
+            let cnt = 0;
+            for(let num of nums){
+                if(num >= i) cnt++
+            }
+            if(cnt == x) return x;
+        }
+        return -1;
+    }
+}
