87cc3a09fc6658ec59b19aef3ab71cdb48b32cda
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-24 23:27:23 -0400

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    reverse(x) {
+        let newX = String(x).split("").reverse();
+        if (newX[newX.length - 1] == "-") {
+            let minus = newX.pop();
+            newX.unshift(minus);
+        }
+        newX = newX.join("");
+        newX = Number(newX);
+        if (newX > Math.pow(2, 31) || newX < Math.pow(-2, 31)) {
+            return 0;
+        } else {
+            return newX;
+        }
+    }
+}
