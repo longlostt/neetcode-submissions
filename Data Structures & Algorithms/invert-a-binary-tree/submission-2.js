6c4aef8770af9697778ed46ac28ccd3ca192b570
longlostt <116051207+longlostt@users.noreply.github.com>
2026-06-29 19:26:26 -0400

    Add: invert-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-2.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        function dfsRevert(root){
+            if (!root) return;
+
+            let temp = root.right;
+            root.right = root.left;
+            root.left = temp;
+            
+            dfsRevert(root.left)
+            dfsRevert(root.right)
+        }
+
+        dfsRevert(root)
+
+        return root;
+    }
+}
