7a1c278f71fd2c8798a42b1fa5c8c855b4940898
longlostt <116051207+longlostt@users.noreply.github.com>
2026-07-06 23:06:52 -0400

    Add: populating-next-right-pointers-in-each-node - submission-7

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-7.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class Node {
+ *     constructor(val = 0, left = null, right = null, next = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} root
+     * @return {Node}
+     */
+    connect(root) {
+        function dfs(root) {
+            if (!root || !root.left) return;
+            root.left.next = root.right;
+            if (root.next) {
+                root.right.next = root.next.left;
+            }
+
+            dfs(root.left);
+            dfs(root.right);
+        }
+        dfs(root);
+        return root;
+    }
+}
