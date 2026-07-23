f087409135738e6bd45ea15d7087c97b8b5eded5
longlostt <116051207+longlostt@users.noreply.github.com>
2026-07-23 00:07:46 -0400

    Add: unionFind - submission-3

---
+++ Data Structures & Algorithms/unionFind/submission-3.js
@@ -0,0 +1,64 @@
+// i enjoyed it very much 9/10;
+
+class UnionFind {
+    /**
+     * @param {number} n
+     */
+    constructor(n) {
+        this.parent = Array.from({ length: n }, (_, index) => index);
+        this.size = new Array(n).fill(1);
+        this.cnt = n; // total root count
+    }
+
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    find(x) {
+        if (this.parent[x] !== this.parent[this.parent[x]]) {
+            // if points to itself, then it's root
+            this.parent[x] = this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {boolean}
+     */
+    isSameComponent(x, y) {
+        return this.find(x) == this.find(y);
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {boolean}
+     */
+    union(x, y) {
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+
+        if (rootX == rootY) return false;
+    
+        let sizeX = this.size[rootX];
+        let sizeY = this.size[rootY];
+        if (sizeX >= sizeY) {
+            this.parent[rootY] = rootX;
+            this.size[rootX] += this.size[rootY];
+        } else {
+            this.parent[rootX] = rootY;
+            this.size[rootY] += this.size[rootX];
+        }
+        this.cnt -= 1;
+        return true;
+    }
+
+    /**
+     * @return {number}
+     */
+    getNumComponents() {
+        return this.cnt;
+    }
+}
