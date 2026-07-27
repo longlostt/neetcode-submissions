ce1f716466722f9a6ee0f706887c4ff3611c8146
longlostt <116051207+longlostt@users.noreply.github.com>
2026-07-26 20:05:47 -0400

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.js
@@ -0,0 +1,41 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.cache = new Map();
+        this.capacity = capacity;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if(this.cache.has(key)){
+            let val = this.cache.get(key);
+            this.cache.delete(key);
+            this.cache.set(key, val);
+            console.log(this.cache);
+            return val;
+        }
+        console.log(this.cache);
+        return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if(this.cache.has(key)){
+            this.cache.delete(key);
+        }
+        if(this.cache.size === this.capacity){
+            this.cache.delete(this.cache.keys().next().value);
+        } 
+        this.cache.set(key, value);
+        console.log(this.cache);
+    }
+}
