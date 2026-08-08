d80e92463160ff7a7a7f753c79e9f6de4fef17ed
longlostt <116051207+longlostt@users.noreply.github.com>
2026-08-08 17:05:52 -0400

    Add: ugly-number - submission-1

---
+++ Data Structures & Algorithms/ugly-number/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isUgly(n) {
+        // if(n < 0) return false;
+        let valid = [2,3,5];
+        function shave(n){
+            if(n == 1) return true;
+            for(let num of valid){
+                if(n % num == 0) return shave(n/num);
+            }
+            return false;
+        }
+        return shave(n)
+        
+    }
+}
