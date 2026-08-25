2ac6cfcd74a19ab645a07c95348329aeacf15073
longlostt <116051207+longlostt@users.noreply.github.com>
2026-08-25 14:24:18 -0400

    Add: longest-substring-without-duplicates - submission-26

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-26.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if(!s) return 0;
+        let seen = new Set();
+        let l = 0;
+        let max = 0;
+        for(let r = 0; r < s.length; r++){
+            if(!seen.has(s[r])) seen.add(s[r]);
+            else{
+                while(seen.has(s[r])){
+                    seen.delete(s[l]);
+                    l++;
+                }
+                seen.add(s[r]);
+            }
+            max = Math.max(max, seen.size);
+        }
+        return max
+    }
+}
