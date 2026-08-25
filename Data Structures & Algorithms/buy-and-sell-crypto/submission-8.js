9410f5abfa3b39234a990733445da3d773317966
longlostt <116051207+longlostt@users.noreply.github.com>
2026-08-25 09:29:54 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let profit = 0;
+        let l = 0;
+        for(let r = 1; r < prices.length; r++){
+            let newProfit = prices[r] - prices[l];
+            while(newProfit < 0){
+                l++;
+                newProfit = prices[r] - prices[l];
+            }
+            profit = Math.max(profit, newProfit);
+
+        }
+        return profit;
+    }
+}
