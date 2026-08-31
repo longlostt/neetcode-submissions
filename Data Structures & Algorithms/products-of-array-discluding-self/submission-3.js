9c963978080252180a969d521d31e72a179d45a0
longlostt <116051207+longlostt@users.noreply.github.com>
2026-08-31 16:46:23 -0400

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = Array.from({length: nums.length}).fill(0);
+        let product = 1;
+
+        for(let i = 0; i < nums.length; i++){
+            res[i] = product;
+            product *= nums[i];
+        }
+
+        product = 1;
+
+        for(let i = nums.length-1; i >= 0; i--){
+            res[i] *= product;
+            product *= nums[i];
+        }
+
+        return res;
+    }
+}
