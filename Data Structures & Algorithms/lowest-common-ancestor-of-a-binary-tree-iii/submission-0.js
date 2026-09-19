50130e8f36cc0595f6337b8246ea092a7e399a20
longlostt <116051207+longlostt@users.noreply.github.com>
2026-09-19 19:33:44 -0400

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-0.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * function Node(val) {
+ *    this.val = val;
+ *    this.left = null;
+ *    this.right = null;
+ *    this.parent = null;
+ * }
+ */
+class Solution {
+    /**
+     * @param {Node} p
+     * @param {Node} q
+     * @return {Node}
+     */
+    constructor(){
+        this.set = new Set();
+    }
+
+    dfs(node){
+        if(!node) return node;
+
+        if(this.set.has(node.val)) return node;
+        this.set.add(node.val);
+
+        return this.dfs(node.parent);
+    }
+
+    lowestCommonAncestor(p, q) {
+        let pUp = this.dfs(p);
+        let qUp = this.dfs(q);
+
+        if(!pUp) return qUp;
+        if(!qUp) return pUp;
+        if(pUp == qUp) return pUp;
+
+    }
+}
