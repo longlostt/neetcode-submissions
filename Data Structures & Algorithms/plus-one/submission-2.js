ca83d95fc2ccd2298d65d72eb3a16d9fc5272357
longlostt <116051207+longlostt@users.noreply.github.com>
2026-09-19 16:35:33 -0400

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+        let carry = false;
+        for (let i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        if(digits[0] == 0) digits.unshift(1);
+        return digits;
+        // console.log(digits);
+    }
+}
